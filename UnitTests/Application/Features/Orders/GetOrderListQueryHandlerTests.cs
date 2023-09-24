a6039228c0f0d7bd5a074bc8725aced315df8718
Muhsin Meydan <meydan1@hotmail.com>
2023-09-25 00:30:34 +0100

    GetOrderList feature - unit tests added - handler added

---
+++ UnitTests/Application/Features/Orders/GetOrderListQueryHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Application.Features.Orders.Common;
+using Application.Features.Orders.GetList;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using static Application.Features.Orders.GetList.GetOrderListQuery;
+
+namespace UnitTests.Application.Features.Orders
+{
+    public class GetOrderListQueryHandlerTests
+    {
+        readonly Mock<IOrderRepository> _orderRepositoryMock;
+        readonly Mock<IMapper> _mapperMock;
+        readonly GetOrderListQueryHandler _handler;
+
+        public GetOrderListQueryHandlerTests()
+        {
+            _orderRepositoryMock = new Mock<IOrderRepository>();
+            _mapperMock = new Mock<IMapper>();
+
+            _handler = new GetOrderListQuery.GetOrderListQueryHandler(
+                _orderRepositoryMock.Object,
+                _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsPaginatedList()
+        {
+            var query = new GetOrderListQuery
+            {
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            var orders = new List<Order>{
+                new Order { Id = Guid.NewGuid(),  ProductId = Guid.NewGuid(), CustomerId = Guid.NewGuid(), },
+                new Order { Id = Guid.NewGuid(),  ProductId = Guid.NewGuid(), CustomerId = Guid.NewGuid(), }
+            };
+            
+            var expectedOrderResponses = orders.Select(customer => new OrderResponse
+            {
+                Id = customer.Id,
+                ProductId = customer.ProductId,
+                CustomerId = customer.CustomerId
+            });
+
+            _mapperMock.Setup(mapper => mapper.ConfigurationProvider)
+                    .Returns(new MapperConfiguration(cfg => cfg.CreateMap<Order, OrderResponse>()));
+
+            _orderRepositoryMock.Setup(repo => repo.GetQueryable())
+                                  .Returns(orders.AsQueryable());
+          
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Data);
+            Assert.Equal(orders.Count, result.Data.Count); 
+            Assert.Equal(query.PageNumber, result.PageNumber);
+        }
+    }
+
+}
---
+++ src/Application/Features/Orders/GetList/GetOrderListQuery.cs
@@ -0,0 +1,50 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Application.Common.Mappings;
+using Application.Common.Models;
+using Application.Features.Orders.Common;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+
+namespace Application.Features.Orders.GetList
+{
+    public class GetOrderListQuery : IRequest<PaginatedList<OrderResponse>>
+    {
+        /// <summary>
+        /// Page number for pagination. Default = 1.
+        /// </summary>
+        /// <example>1</example>
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Number of items per page for pagination. Default =10
+        /// </summary>
+        /// <example>10</example>
+        public int PageSize { get; set; } = 10;
+
+        public GetOrderListQuery() { }
+
+        public class GetOrderListQueryHandler : IRequestHandler<GetOrderListQuery, PaginatedList<OrderResponse>>
+        {
+            readonly IOrderRepository _orderRepository;
+            readonly IMapper _mapper;
+
+            public GetOrderListQueryHandler(IOrderRepository customerRepository, IMapper mapper)
+            {
+                _orderRepository = customerRepository;
+                _mapper = mapper;
+            }
+
+            public Task<PaginatedList<OrderResponse>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+            {
+                var result = _orderRepository.GetQueryable()
+                 .ProjectTo<OrderResponse>(_mapper.ConfigurationProvider)
+                 .PaginatedList(request.PageNumber, request.PageSize);
+
+                return Task.FromResult(result);
+            }
+        }
+
+    }
+
+}
