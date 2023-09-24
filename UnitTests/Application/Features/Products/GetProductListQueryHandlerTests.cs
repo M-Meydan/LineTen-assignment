a02e7bf5f5d650db24ebb58e3d7c44beca6b524a
Muhsin Meydan <meydan1@hotmail.com>
2023-09-24 22:07:01 +0100

    Get product list feature implemented -unitest added -handler added

---
+++ UnitTests/Application/Features/Products/GetProductListQueryHandlerTests.cs
@@ -0,0 +1,65 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Application.Features.Products.Common;
+using Application.Features.Products.GetList;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using static Application.Features.Products.GetList.GetProductListQuery;
+
+namespace UnitTests.Application.Features.Products
+{
+    public class GetProductListQueryHandlerTests
+    {
+        readonly Mock<IProductRepository> _productRepositoryMock;
+        readonly Mock<IMapper> _mapperMock;
+        readonly GetProductListQueryHandler _handler;
+
+        public GetProductListQueryHandlerTests()
+        {
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _mapperMock = new Mock<IMapper>();
+
+            _handler = new GetProductListQuery.GetProductListQueryHandler(
+                _productRepositoryMock.Object,
+                _mapperMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsPaginatedList()
+        {
+            var query = new GetProductListQuery
+            {
+                PageNumber = 1,
+                PageSize = 10
+            };
+
+            var products = new List<Product>{
+                new Product { Id = Guid.NewGuid(), Name = "Sample Product1", Description = "Sample Description1" },
+                new Product { Id = Guid.NewGuid(),  Name = "Sample Product2",Description = "Sample Description2" }
+            };
+            
+            var expectedProductResponses = products.Select(product => new ProductResponse
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description
+            });
+
+            _mapperMock.Setup(mapper => mapper.ConfigurationProvider)
+                    .Returns(new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductResponse>()));
+
+            _productRepositoryMock.Setup(repo => repo.GetQueryable())
+                                  .Returns(products.AsQueryable());
+          
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Data);
+            Assert.Equal(products.Count, result.Data.Count); 
+            Assert.Equal(query.PageNumber, result.PageNumber);
+        }
+    }
+
+}
---
+++ src/Application/Features/Products/GetList/GetProductListQuery.cs
@@ -0,0 +1,50 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Application.Common.Mappings;
+using Application.Common.Models;
+using Application.Features.Products.Common;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+
+namespace Application.Features.Products.GetList
+{
+    public class GetProductListQuery : IRequest<PaginatedList<ProductResponse>>
+    {
+        /// <summary>
+        /// Page number for pagination. Default = 1.
+        /// </summary>
+        /// <example>1</example>
+        public int PageNumber { get; set; } = 1;
+
+        /// <summary>
+        /// Number of items per page for pagination. Default =10
+        /// </summary>
+        /// <example>10</example>
+        public int PageSize { get; set; } = 10;
+
+        public GetProductListQuery() { }
+
+        public class GetProductListQueryHandler : IRequestHandler<GetProductListQuery, PaginatedList<ProductResponse>>
+        {
+            readonly IProductRepository _productRepository;
+            readonly IMapper _mapper;
+
+            public GetProductListQueryHandler(IProductRepository customerRepository, IMapper mapper)
+            {
+                _productRepository = customerRepository;
+                _mapper = mapper;
+            }
+
+            public Task<PaginatedList<ProductResponse>> Handle(GetProductListQuery request, CancellationToken cancellationToken)
+            {
+                var result = _productRepository.GetQueryable()
+                 .ProjectTo<ProductResponse>(_mapper.ConfigurationProvider)
+                 .PaginatedList(request.PageNumber, request.PageSize);
+
+                return Task.FromResult(result);
+            }
+        }
+
+    }
+
+}
