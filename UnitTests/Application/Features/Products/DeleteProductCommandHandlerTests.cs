e86785b5f091eb54b589a075a538bd65f5004809
Muhsin Meydan <meydan1@hotmail.com>
2023-09-24 21:47:25 +0100

    Delete product feature implemented -unit test added -handler added

---
+++ UnitTests/Application/Features/Products/DeleteProductCommandHandlerTests.cs
@@ -0,0 +1,62 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Application.Features.Products.Delete;
+using Domain.Entities;
+using Moq;
+using static Application.Features.Products.Delete.DeleteProductCommand;
+
+namespace UnitTests.Application.Features.Products
+{
+    public class DeleteProductCommandHandlerTests
+    {
+        readonly Mock<IProductRepository> _productRepositoryMock;
+        readonly DeleteProductHandler _handler;
+
+        public DeleteProductCommandHandlerTests()
+        {
+            _productRepositoryMock = new Mock<IProductRepository>();
+
+            _handler = new DeleteProductHandler(_productRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsDeletedProduct_WhenProductExist()
+        {
+            var id = Guid.NewGuid();
+            var deleteRequest = new DeleteProductCommand { Id = id };
+
+            var existingProduct = new Product
+            {
+                Id = id,
+                Name = "Sample Product",
+                Description = "Sample Description",
+                SKU = 12345
+            };
+
+            _productRepositoryMock.Setup(repo => repo.GetByIdAsync(id, It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync(existingProduct);
+
+            // Act
+            var result = await _handler.Handle(deleteRequest, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(existingProduct, result);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsNull_WhenProductNotExist()
+        {
+            // Arrange
+            var productId = Guid.NewGuid();
+            _productRepositoryMock.Setup(repo => repo.GetByIdAsync(productId, It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync((Product)null);
+
+            // Act
+            var result = await _handler.Handle(new DeleteProductCommand { Id = productId }, CancellationToken.None);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+    }
+}
---
+++ src/Application/Features/Products/Delete/DeleteProductCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Products.Delete
+{
+    public class DeleteProductCommand : IRequest<Product>
+    {
+        public Guid Id { get; set; }
+
+        public class DeleteProductHandler : IRequestHandler<DeleteProductCommand, Product>
+        {
+            private readonly IProductRepository _productRepository;
+
+            public DeleteProductHandler(IProductRepository productRepository)
+            {
+                _productRepository = productRepository;
+            }
+
+            public async Task<Product> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+            {
+                var product = await _productRepository.GetByIdAsync(request.Id, cancellationToken);
+
+                if (product != null)
+                    await _productRepository.DeleteAsync(product);
+
+                return product;
+            }
+        }
+    }
+}
