1aca6508e8bf47baef3cf09f8bafa6c5c6414ef9
Muhsin Meydan <meydan1@hotmail.com>
2023-09-25 00:31:40 +0100

    Delete order feature -unit test added -handler added

---
+++ src/Application/Features/Orders/Delete/DeleteOrderCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Orders.Delete
+{
+    public class DeleteOrderCommand : IRequest<Order>
+    {
+        public Guid Id { get; set; }
+
+        public class DeleteOrderHandler : IRequestHandler<DeleteOrderCommand, Order>
+        {
+            private readonly IOrderRepository _orderRepository;
+
+            public DeleteOrderHandler(IOrderRepository orderRepository)
+            {
+                _orderRepository = orderRepository;
+            }
+
+            public async Task<Order> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+            {
+                var order = await _orderRepository.GetByIdAsync(request.Id, cancellationToken);
+
+                if (order != null)
+                    await _orderRepository.DeleteAsync(order);
+
+                return order;
+            }
+        }
+    }
+}
