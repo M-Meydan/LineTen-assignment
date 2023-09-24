98afdfafde565a3c83ee07458111dae880eca1e1
Muhsin Meydan <meydan1@hotmail.com>
2023-09-24 20:23:24 +0100

    ProductConfiguration added

---
+++ src/Infrastructure/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
+namespace Infrastructure.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                    .HasColumnOrder(1)
+                    .ValueGeneratedOnAdd()
+                    .HasValueGenerator<SequentialGuidValueGenerator>();
+        }
+    }
+}
