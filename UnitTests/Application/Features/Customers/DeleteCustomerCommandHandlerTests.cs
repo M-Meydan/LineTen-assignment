27c0d635d7c9d7f4d145f31538dab944d4697706
Muhsin Meydan <meydan1@hotmail.com>
2023-09-24 19:38:53 +0100

    DeleteCustomer feature implemented - unit test added - handler implemented

---
+++ UnitTests/Application/Features/Customers/DeleteCustomerCommandHandlerTests.cs
@@ -0,0 +1,66 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Application.Features.Customers.Common;
+using Application.Features.Customers.Create;
+using Application.Features.Customers.Delete;
+using Domain.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+
+namespace UnitTests.Application.Features.Customers
+{
+    public class DeleteCustomerCommandHandlerTests
+    {
+        readonly Mock<ICustomerRepository> _customerRepositoryMock;
+        readonly DeleteCustomerCommand.DeleteCustomerHandler _handler;
+
+        public DeleteCustomerCommandHandlerTests()
+        {
+            _customerRepositoryMock = new Mock<ICustomerRepository>();
+
+            _handler = new DeleteCustomerCommand.DeleteCustomerHandler(_customerRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsDeletedCustomer_WhenCustomerExist()
+        {
+            var customerId = Guid.NewGuid();
+            var deleteRequest = new DeleteCustomerCommand { Id = customerId };
+
+            var existingCustomer = new Customer
+            {
+                Id = customerId,
+                FirstName = "James",
+                LastName = "Bond",
+                Phone = 1234567890,
+                Email = "James@example.com"
+            };
+
+            _customerRepositoryMock.Setup(repo => repo.GetByIdAsync(customerId, It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync(existingCustomer);
+
+            // Act
+            var result = await _handler.Handle(deleteRequest, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(existingCustomer, result);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsNull_WhenCustomerNotExist()
+        {
+            // Arrange
+            var customerId = Guid.NewGuid();
+            _customerRepositoryMock.Setup(repo => repo.GetByIdAsync(customerId, It.IsAny<CancellationToken>()))
+                                  .ReturnsAsync((Customer)null);
+
+            // Act
+            var result = await _handler.Handle(new DeleteCustomerCommand { Id = customerId }, CancellationToken.None);
+
+            // Assert
+            Assert.Null(result); // Check if the result is null for a non-existing customer
+        }
+
+    }
+}
---
+++ src/Application/Features/Customers/Delete/DeleteCustomerCommand.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Interfaces.Infrastructure.Persistence.Repositories;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Customers.Delete
+{
+    public class DeleteCustomerCommand : IRequest<Customer>
+    {
+        public Guid Id { get; set; }
+
+        public class DeleteCustomerHandler : IRequestHandler<DeleteCustomerCommand, Customer>
+        {
+            private readonly ICustomerRepository _customerRepository;
+
+            public DeleteCustomerHandler(ICustomerRepository customerRepository)
+            {
+                _customerRepository = customerRepository;
+            }
+
+            public async Task<Customer> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+            {
+                var customer = await _customerRepository.GetByIdAsync(request.Id, cancellationToken);
+
+                if (customer != null)
+                    await _customerRepository.DeleteAsync(customer);
+
+                return customer;
+            }
+        }
+    }
+}
